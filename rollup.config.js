1409062c4ee4caf0a6c6e6de598c38f83f437c48
Augustin Chassine <external.z09achas@decathlon.com>
2019-10-07 23:47:25 +0200

    feat: dependencies and build for augmented markdown

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
+import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
+import { mdsvex } from 'mdsvex';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -14,6 +16,19 @@ const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
 
+
+const svx = mdsvex({
+	extension: '.md', 
+	//layout: path.join(__dirname, 'src/routes/_layout.svelte'), // this needs to be an absolute path
+	//parser: md => md.use(SomePlugin), // you can add markdown-it plugins if the feeling takes you
+	// you can add markdown-it options here, html is always true
+	markdownOptions: {
+	  typographer: true,
+	  linkify: true,
+	  //highlight: (str, lang) => whatever(str, lang), // this should be a real function if you want to highlight
+	},
+  });
+
 export default {
 	client: {
 		input: config.client.input(),
@@ -23,10 +38,13 @@ export default {
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
+			json(),
 			svelte({
 				dev,
+				extensions: ['.svelte', '.md', '.svx'],
 				hydratable: true,
-				emitCss: true
+				emitCss: true,
+				preprocess: [svx]
 			}),
 			resolve({
 				browser: true,
@@ -67,9 +85,12 @@ export default {
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
+			json(),
 			svelte({
+				extensions: ['.svelte', '.md', '.svx'],
 				generate: 'ssr',
-				dev
+				dev,
+				preprocess: svx
 			}),
 			resolve({
 				dedupe
